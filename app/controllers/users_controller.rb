15ecfcfccc94741ea726240db795be8b9fbd90cd
Jordan Youngs <jordankyoungs@gmail.com>
2018-09-13 17:53:22 +0000

    users#create action successfully adds new users to database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,27 @@ class UsersController < ApplicationController
 
   def new
     # @user = User.new
+    @users = User.all
   end
 
   def create
-    @user = User.new
+    @user = User.new(user_params)
+    if @user.save
+      flash.now.notice = "success!"
+    end
+    redirect_to '/signup'
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation
+      )
+  end
+
 end
