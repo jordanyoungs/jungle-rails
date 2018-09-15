808f7f337afd96b3fdb9acc4a1f6df6ded90dcd9
Jordan Youngs <jordankyoungs@gmail.com>
2018-09-15 02:18:49 +0000

    Ratings are almost fully functioning

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+  def create
+    @product = Product.find_by(id: params.require(:product_id))
+    @review = @product.reviews.new(review_params)
+
+    @review.user = current_user unless current_user.nil?
+
+    if @review.save
+      flash[:success] = "Thanks for the Review!"
+      @product.update(average_rating: @product.reviews.average(:rating).round)
+      redirect_to @product
+    else
+      render 'show'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
+end
