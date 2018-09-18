e90bcbbfd3d4c4cb0feb3514f5ed42e04c0dc7f4
Jordan Youngs <jordankyoungs@gmail.com>
2018-09-18 00:44:47 +0000

    completed validation tests for procut model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,43 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    before
+    before(:each) do
+      @category = Category.new
+      @product = Product.new({
+        name: 'product_name',
+        price: 100,
+        quantity: 10,
+        category: @category
+      })
+    end
+
+    it "is valid with valid attributes" do
+      expect(@product).to be_valid
+      expect(@product.errors).to be_empty
+    end
+
+    it "is not valid without a name" do
+      @product.name = nil
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it "is not valid without a price" do
+      @product.price_cents = nil
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it "is not valid without a quantity" do
+      @product.quantity = nil
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it "is not valid without a category" do
+      @product.category = nil
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
   end
 end
